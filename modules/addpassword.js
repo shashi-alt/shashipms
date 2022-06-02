cfeb4272b1fdf4a0e38bf211b44f3b44eafd57e6
shashi ranjan kumar <66456239+shashi-alt@users.noreply.github.com>
2022-06-02 18:52:34 +0530

    shashiapp

--- modules/addpassword.js
+++ modules/addpassword.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.set('useUnifiedTopology', true);
-mongoose.connect('mongodb+srv://srk694307:Srk694307@cluster0.zghds.mongodb.net/pms?retryWrites=true&w=majority', { useNewUrlParser: true, useCreateIndex: true });
+mongoose.connect('mongodb+srv://srk694307:pProhSA8wkNe7zy4@cluster0.zghds.mongodb.net/pms?retryWrites=true&w=majority', { useNewUrlParser: true, useCreateIndex: true });
 var conn = mongoose.Collection;
 var addpassSchema = new mongoose.Schema({
     password_category: {
