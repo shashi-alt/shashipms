0cec0f27f6f879d2ba554d04f92447925f8a4e83
shashi ranjan kumar <66456239+shashi-alt@users.noreply.github.com>
2022-06-02 18:37:15 +0530

    shashiapp

--- modules/user.js
+++ modules/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.set('useUnifiedTopology', true);
-mongoose.connect('mongodb://localhost:27017/pms', {useNewUrlParser: true,useCreateIndex: true});
+mongoose.connect('mongodb+srv://srk694307:Srk694307@cluster0.zghds.mongodb.net/?retryWrites=true&w=majority', {useNewUrlParser: true,useCreateIndex: true});
 var conn =mongoose.Collection;
 var userSchema =new mongoose.Schema({
     username: {type:String, 
