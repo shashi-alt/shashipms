64bd3904beea9d4274331a13d5a9f142a7677f7b
shashi ranjan kumar <66456239+shashi-alt@users.noreply.github.com>
2022-06-02 18:43:06 +0530

    shashiapp

--- modules/password_category.js
+++ modules/password_category.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.set('useUnifiedTopology', true);
-mongoose.connect('mongodb+srv://srk694307:Srk694307@cluster0.zghds.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useCreateIndex: true });
+mongoose.connect('mongodb+srv://srk694307:Srk694307@cluster0.zghds.mongodb.net/pms?retryWrites=true&w=majority', { useNewUrlParser: true, useCreateIndex: true });
 var conn = mongoose.Collection;
 var passcatSchema = new mongoose.Schema({
     password_category: {
